2a64e319f87f249d077392b5f14322f01f0fbbff
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-18 19:24:09 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE series (
+id INTEGER PRIMARY KEY,
+title TEXT,
+author TEXT,
+sub-genre TEXT);
+
+CREATE TABLE sub-genre(
+id INTEGER PRIMARY KEY,
+name TEXT);
+
+CREATE TABLE authors (
+id INTEGER PRIMARY KEY,
+name TEXT);
+
+CREATE TABLE books (
+id INTEGER PRIMARY KEY,
+title TEXT,
+year INTEGER,
+)
