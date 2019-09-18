ae850499a5132fc4fca0aafadee85af2d4c56ce1
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-18 20:53:46 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series INNER JOIN authors ON authors.id = series.author.id INNER JOIN characters ON authors.id = characters.authors_id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(characters.species) DESC
-  LIMIT 1";"
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
